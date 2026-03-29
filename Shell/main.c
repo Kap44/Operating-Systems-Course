bda3e0f9bbc7e4ce94e7f8724a51eb90217e3b55
Kap44 <71781246+Kap44@users.noreply.github.com>
2026-03-29 18:03:28 +0300

    Enhance main.c to include user name in prompt
    Added user name retrieval and updated command prompt function.

--- Shell/main.c
+++ Shell/main.c
@@ -1,17 +1,23 @@
 #include "basic_funcs.h"
 
-#define MAXCMDINPUT 10000
-char *cmd_input;
+char *cmd_input; // string that stores the input of the user in command prompt
+char *user; // string that stores the name of the user
 
 int main() { 
+    char *cmd_input; // string that stores the input of the user in command prompt
+    char *user = getenv("LOGNAME"); // string that stores the name of the user
     while(1){
-        command_prompt();
+        command_prompt(user);
         cmd_input = malloc(MAXCMDINPUT);
-		fgets(cmd_input,MAXCMDINPUT,stdin);
+        fgets(cmd_input,MAXCMDINPUT,stdin); // read and store the input of the user
+
+        // replace the new line that exists in the end of the input, with null terminator
         if((strlen(cmd_input) > 0) && (cmd_input[strlen(cmd_input)-1] == '\n')){
-            cmd_input[strlen(cmd_input)-1] = ' ';
+            cmd_input[strlen(cmd_input)-1] = '\0';
         }
-		read_command(cmd_input);
+
+        // parse
+        read_command(cmd_input);
     }
     return 0; 
 }
