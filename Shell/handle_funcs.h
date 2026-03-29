dded334656f0763ce956eca8c32cdd0057d6842c
Kap44 <71781246+Kap44@users.noreply.github.com>
2026-03-29 18:03:04 +0300

    Update handle_funcs.h with new function declarations
    Added function declarations for global variable handling and redirection.

--- Shell/handle_funcs.h
+++ Shell/handle_funcs.h
@@ -4,11 +4,19 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
-void pipes_handler(char **args);
+/* Function that sets or gets a globar variable.
+   if mode == 0: set global variable -> Its arguments contains the names and the values of the global variables that will be set.
+   if mode == 1: get global variable -> Its arguments contains the names of the global variables that will be get */
+char **global_var_handler(char **args, int mode);
 
-void input_redirection_handler(char **args);
+/* Receives as input the commands of each pipe and executes them */
+int pipes_handler(char **args);
 
-void output_simple_redirection_handler(char **args);
+/* Receives as input the commands and the file names and performs input redirection. 
+Forked = 1 if there is already a child running and the function is careful not to make unnecessary forks*/
+int input_redirection_handler(char **args, int forked);
 
-void output_double_redirection_handler(char **args);
+/* Receives as input the commands and the file names and performs output redirection */
+int output_redirection_handler(char **args);
